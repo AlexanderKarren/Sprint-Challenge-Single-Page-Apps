a25eb0d7f3e05e5bfac1dd5cb273367ba8f48a2e
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-28 10:13:09 -0700

    added styling to cards, successfully displaying characters from API

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Container = styled.div`
+  box-shadow: 4px 4px 4px 0px rgba(0,0,0,0.75);
+  display:flex;
+  width:45%;
+  margin: 20px 0;
+  background-color:#F0F8FF;
+`
+
+const Information = styled.div`
+  padding:20px;
+`
+
+const CardHeading = styled.h2`
+  font-size:150%;
+  font-weight:bold;
+`
+
+const CharacterCard = ({ character }) => {
+  return (
+    <Container>
+      <img src={character.image} alt="character" />
+      <Information>
+        <CardHeading>{character.name}</CardHeading>
+        <p><span>Species:&nbsp;</span>{character.species}</p>
+        <p><span>Status:&nbsp;</span>{character.status}</p>
+        <p><span>Gender:&nbsp;</span>{character.gender}</p>
+      </Information>
+    </Container>
+  )
 }
+
+export default CharacterCard;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color:black;
+  color:white;
+  width:100%;
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  padding: 0 20px;
+`
+
+const MainHeading = styled.h1`
+  font-family: 'Trade Winds', sans-serif;
+  font-size:200%;
+`
+
+const Links = styled.div`
+  width:20%;
+  display:flex;
+  justify-content:space-between;
+`
+
+const linkStyle = {
+  textDecoration: "none",
+  color: "inherit",
+}
 
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-      <div className="links-container">
+    <StyledHeader className="ui centered">
+      <MainHeading className="ui center">Rick &amp; Morty Fan Page</MainHeading>
+      <Links>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/characters">Characters</NavLink>
         <NavLink to="/locations">Locations</NavLink>
-      </div>
-    </header>
+      </Links>
+    </StyledHeader>
   );
 }
