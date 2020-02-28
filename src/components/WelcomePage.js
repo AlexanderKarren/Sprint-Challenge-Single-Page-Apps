00da30042fa875c1ad637bb5fe32640b79f01b8f
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-28 11:52:07 -0700

    adjustments to welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
+const Container = styled.div`
+  background-color:#F0FFF0;
+  box-shadow: 4px 4px 4px 0px rgba(0,0,0,0.75);
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  padding:30px;
+`
+
+const Heading = styled.h2`
+  font-size:150%;
+  font-weight:bold;
+`
+
+const Links = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  margin:20px;
+`
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
-        <h1>Welcome to the ultimate fan site!</h1>
+      <Container>
+        <Heading>ERP! It-it-it it's the Rick and Morty API, Morty!</Heading>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-      </header>
+        <Links>
+          <NavLink className="card-link" to="/characters">Characters</NavLink>
+          <NavLink className="card-link" to="/locations">Locations</NavLink>
+          <NavLink className="card-link" to="/episodes">Episodes</NavLink>
+        </Links>
+      </Container>
     </section>
   );
 }
